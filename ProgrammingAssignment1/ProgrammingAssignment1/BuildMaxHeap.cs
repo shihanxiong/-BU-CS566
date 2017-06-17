191d6db420435ebf143040f51cf962fa95ca7540
shihanxiong <shihanxiong@gmail.com>
2017-06-16 20:45:11 -0400

    Init.

---
+++ ProgrammingAssignment1/ProgrammingAssignment1/BuildMaxHeap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgrammingAssignment1
+{
+    public class BuildMaxHeap
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("CS566 - Programming Assignment 1");
+
+
+
+            Console.WriteLine("\nPress any key to close... ");
+            Console.ReadLine();
+        }
+    }
+}
