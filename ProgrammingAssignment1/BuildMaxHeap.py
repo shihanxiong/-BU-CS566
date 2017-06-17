db64736b289559c68b32b7d22c7be3f23dc33187
shihanxiong <shihanxiong@gmail.com>
2017-06-16 22:32:26 -0400

    Added input files and created function to read input files into int array.

--- ProgrammingAssignment1/BuildMaxHeap.py
+++ ProgrammingAssignment1/BuildMaxHeap.py
@@ -3,6 +3,14 @@ CS566 - Programming Assignment 1
 Hanxiong Shi
 hanxiong@bu.edu
 """
-print("CS566 - Programming Assignment 1")
+print("\nCS566 - Programming Assignment 1")
 print("Hanxiong Shi")
 print("hanxiong@bu.edu\n")
+
+def read_integers(filename):
+    """
+    Parse input files into int array
+    """
+    with open(filename) as f:
+        return [int(x) for x in f]
+
